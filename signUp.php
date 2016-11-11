bc4d08ccc8c6864090e9d59998e599ceaac5e22a
Zayed Bin Maleque (News) <ne.zayed@gmail.com>
2016-11-11 21:24:32 +0600

    Create signUp.php

---
+++ signUp.php
@@ -0,0 +1,118 @@
+<?php
+?>
+<form action="what ever place you want to send.php" method="post">
+<h1>Sign UP</h1>
+First Name :
+<input value="" type="text" name="fname" /><br>
+Last Name :
+<input value="" type="text" name=" lname" /><br>
+DOB :
+<select id="month" name="month">                      
+<option value="0">--Select Month--</option>
+<option value="1">Jan</option>
+<option value="2">Feb</option>
+<option value="3">Mar</option>
+<option value="4">Apr</option>
+<option value="5">May</option>
+<option value="6">Jun</option>
+<option value="7">Jul</option>
+<option value="8">Aug</option>
+<option value="9">Sep</option>
+<option value="10">Oct</option>
+<option value="11">Nov</option>
+<option value="12">Dec</option>
+</select>
+<select id="date" name="date">                      
+<option value="0">--Select Date--</option>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option>
+<option value="9">9</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+<option value="13">13</option>
+<option value="14">14</option>
+<option value="15">15</option>
+<option value="16">16</option>
+<option value="17">17</option>
+<option value="18">18</option>
+<option value="19">19</option>
+<option value="20">20</option>
+<option value="21">21</option>
+<option value="22">22</option>
+<option value="23">23</option>
+<option value="24">24</option>
+<option value="25">25</option>
+<option value="26">26</option>
+<option value="27">27</option>
+<option value="28">28</option>
+<option value="29">29</option>
+<option value="30">30</option>
+<option value="31">31</option>
+</select>
+<select id="year" name="year">                      
+<option value="0">--Select Year--</option>
+<option value="1">1967</option>
+<option value="2">1968</option>
+<option value="3">1969</option>
+<option value="4">1970</option>
+<option value="5">1971</option>
+<option value="6">1972</option>
+<option value="7">1973</option>
+<option value="8">1974</option>
+<option value="9">1975</option>
+<option value="10">1976</option>
+<option value="11">1977</option>
+<option value="12">1978</option>
+<option value="13">1979</option>
+<option value="14">1980</option>
+<option value="15">1981</option>
+<option value="16">1982</option>
+<option value="17">1983</option>
+<option value="18">1984</option>
+<option value="19">1985</option>
+<option value="20">1986</option>
+<option value="21">1987</option>
+<option value="22">1988</option>
+<option value="23">1989</option>
+<option value="24">1990</option>
+<option value="25">1991</option>
+<option value="26">1992</option>
+<option value="27">1993</option>
+<option value="28">1994</option>
+<option value="29">1995</option>
+<option value="30">1996</option>
+<option value="31">1997</option>
+</select><br>
+
+Gender :
+<Input type = 'Radio' Name ='gender' value= 'male'>Male
+<Input type = 'Radio' Name ='gender' value= 'female' >Female
+<Input type = 'Radio' Name ='gender' value= 'other' >Other<br>
+
+Phone :
+<input value="+880" type="text" name="phone" /><br>
+
+Email :
+<input value="" type="text" name="email" /><br>
+
+Password  : <input type="password" name="pass" /><br>
+
+Confirm Password  : <input type="password" name="cpass" /><br>
+
+Select image to upload:
+<input type="file" name="filename" >
+  
+  
+<br>
+<input type="submit" value="Submit" /><br>
+
+<a href="index.html">Back</a>
+
+</form>
