2a77489da6e38becb9ffca9ca3ff2c544713f326
Zayed Bin Maleque (News) <ne.zayed@gmail.com>
2016-11-11 21:18:19 +0600

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<form action="logoinCheck.php" method="post">
+<h1>LOGIN</h1>
+User Name :
+<input value="" type="text" name="uname" /><br>
+Password  : <input type="password" name="pass" /><br>
+
+<br>
+<input type="submit" value="Login" /><br>
+<a href="index.html">Back</a>
+
+</form>
